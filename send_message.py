30953ee1d19950a96d9e736a6df909556f45095a
Lewis Halstead <108507629+Lewish1998@users.noreply.github.com>
2023-06-10 13:53:22 +0100

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,12 +1,9 @@
 import os
 import pickle
-# Gmail API utils
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-# for encoding/decoding messages in base64
 from base64 import urlsafe_b64decode, urlsafe_b64encode
-# for dealing with attachement MIME types
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
@@ -15,36 +12,29 @@ from email.mime.base import MIMEBase
 from mimetypes import guess_type as guess_mime_type
 
 
-# Request all access (permission to read/send/receive emails, manage the inbox, and more)
 SCOPES = ['https://mail.google.com/']
 our_email = os.getenv('EMAIL')
 
 
 def gmail_authenticate():
     creds = None
-    # the file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time
     if os.path.exists("token.pickle"):
         with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
-    # if there are no (valid) credentials availablle, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # save the credentials for the next run
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
     return build('gmail', 'v1', credentials=creds)
 
 
-# get the Gmail API service
 service = gmail_authenticate()
 
 
-# Adds the attachment with the given filename to the given message
 def add_attachment(message, filename):
     content_type, encoding = guess_mime_type(filename)
     if content_type is None or encoding is not None:
@@ -73,7 +63,7 @@ def add_attachment(message, filename):
 
 
 def build_message(destinations, obj, body, attachments=[]):
-    if not attachments: # no attachments given
+    if not attachments:
         message = MIMEText(body)
         message['to'] = ', '.join(destinations)
         message['from'] = our_email
@@ -88,30 +78,9 @@ def build_message(destinations, obj, body, attachments=[]):
             add_attachment(message, filename)
     return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
 
-# def build_message(destination, obj, body, attachments=[]):
-#     if not attachments: # no attachments given
-#         message = MIMEText(body)
-#         message['to'] = destination
-#         message['from'] = our_email
-#         message['subject'] = obj
-#     else:
-#         message = MIMEMultipart()
-#         message['to'] = destination
-#         message['from'] = our_email
-#         message['subject'] = obj
-#         message.attach(MIMEText(body))
-#         for filename in attachments:
-#             add_attachment(message, filename)
-#     return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
-
 
 def send_message(service, destination, obj, body, attachments=[]):
     return service.users().messages().send(
       userId="me",
       body=build_message(destination, obj, body, attachments)
     ).execute()
-
-
-
-
-
